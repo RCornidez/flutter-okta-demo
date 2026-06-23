e1e7e6b34458110a20e1d6231336b4e0784e979d
Rodrigo Cornidez <cornidez04@gmail.com>
2026-06-22 19:20:21 -0700

    fix app name format

--- demo/android/app/build.gradle.kts
+++ demo/android/app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.okta.trial-8077266"
+        applicationId = "com.okta.trial_8077266"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
