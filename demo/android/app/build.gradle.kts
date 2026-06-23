8bfe6eb00d306ba3093e8af219383a7873da2600
Rodrigo Cornidez <cornidez04@gmail.com>
2026-06-22 18:40:13 -0700

    Okta redirects: Sign-in redirect URI: com.okta.trial-8077266:/callback Sign-out redirect URI: com.okta.trial-8077266:/
    Changes to handle redirects to app:
    1. android/app/build.gradle.kts - applicationId set to com.okta.trial-8077266, namespace set to com.okta.trial_8077266
    2. android/app/src/main/AndroidManifest.xml - added OAuth redirect intent filter with scheme com.okta.trial-8077266
    3. ios/Runner/Info.plist - added CFBundleURLTypes with URL scheme com.okta.trial-8077266
    4. ios/Runner.xcodeproj/project.pbxproj - updated PRODUCT_BUNDLE_IDENTIFIER to com.okta.trial-8077266 for Debug, Release, and Profile build configs

--- demo/android/app/build.gradle.kts
+++ demo/android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.demo"
+    namespace = "com.okta.trial_8077266"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.demo"
+        applicationId = "com.okta.trial-8077266"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
